db7872f95cf1bae1bdbb73023d99a584edf24317
Jerrod Long <jlong@doeanderson.com>
2019-10-04 22:05:40 -0400

    [fix] Fixed hero styling.

--- src/js/components/FixedHero.vue
+++ src/js/components/FixedHero.vue
@@ -1,7 +1,6 @@
 <template>
   <div
     :style="background"
-
     class="fixed-hero"
   >
     <figure ref="hero">
@@ -52,8 +51,21 @@
     },
     mounted() {
       this.$nextTick(() => {
-        this.$emit('set-hero-offset', this.$el.offsetHeight);
-      })
+        window.addEventListener('resize', this.getHeroHeight);
+        this.getHeroHeight();
+      });
+    },
+    destroyed() {
+      window.removeEventListener('resize', this.getHeroHeight);
+    },
+    methods: {
+      getHeroHeight() {
+        const header = document.getElementsByClassName('header')[0].offsetHeight;
+        const heroHeight = window.innerHeight - header;
+        // debugger;
+        // const heroHeight = this.$el.offsetHeight;
+        return this.$emit('set-hero-offset', heroHeight);
+      }
     }
   };
 </script>
@@ -65,8 +77,10 @@
     padding: $u6;
     position: fixed;
     width: 100%;
-    top: 14.8rem;
+    top: 10rem;
     left: 0;
+    bottom: 0;
+    right:0;
 
     img {
       display: none;
@@ -96,6 +110,14 @@
       @include font-primary;
       height: 5rem;
       font-size: 1.8rem;
+    }
+
+    @include tablet() {
+      top: 13.6rem;
+    }
+
+    @include desktop() {
+      top: 14.6rem;
     }
 
     @include cinema() {
