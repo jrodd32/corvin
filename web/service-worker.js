1b186d6d9a0cd5c97ad19c7b699c39c58ea82f03
Jerrod Long <jlong@doeanderson.com>
2020-02-18 00:26:25 -0500

    :memo:

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,9 +1,9 @@
-importScripts("workbox-sw.js", "/static/precache-manifest.9f8d2ecc741ade91bd241efd85e972fe.js");
+importScripts("workbox-sw.js", "/static/precache-manifest.e6331e2907ba27f7d92f12f45bec4373.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
 const cacheName = 'BaseSite';
-const cacheVersion = '1581793522237ceddca85f5349c6ea5fd';
+const cacheVersion = '1581998815005b00a8742690d85c90b08';
 const cacheKey = cacheName + cacheVersion;
 const urlsToCache = self.__precacheManifest.map(cacheItem => cacheItem.url);
 
---
+++ web/static/precache-manifest.e6331e2907ba27f7d92f12f45bec4373.js
@@ -0,0 +1,56 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/fonts/icomoon.26bcd5c12d221fc377a3721b62e9c829.woff2"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/fonts/icomoon.8b75d1cbc569367247904547c69c0cd6.woff"
+  },
+  {
+    "revision": "c33e0ac9900891db9c1f98dd2fa5a070",
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/index.html"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/main.8c29fbde1e74f6c11047.js"
+  },
+  {
+    "revision": "4407108eef93eec6356222f1eea930d6",
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/manifest.json"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/page-about.3268ce7d3a824a968730.js"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/page-contact.6111419a00a69e6348c6.js"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/page-errors.477462b1667f0a56d069.js"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/page-flooring.46c3e90535599b9a18ec.js"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/page-gallery.7d03a32c705f5955a0a8.js"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/page-home.265f8810a391a80d7ba4.js"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/page-store.22f9291c8cd400bcce75.js"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/styles.150d71eb16becbb4e295.css"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/styles.150d71eb16becbb4e295.js"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/vendor.17bd81c5fdabb1ade197.css"
+  },
+  {
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/vendor.17bd81c5fdabb1ade197.js"
+  },
+  {
+    "revision": "e07ce02fca8c037795fc6136bcee1b14",
+    "url": "https://corvin.s3.us-east-2.amazonaws.com/static/workbox-sw.js"
+  }
+]);
