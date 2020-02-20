e9a4e227560bbbbe562300a841f85b7157a96385
Jerrod Long <jlong@doeanderson.com>
2020-02-19 19:41:48 -0500

    :memo:

--- web/service-worker.js
+++ web/service-worker.js
@@ -3,7 +3,7 @@ importScripts("workbox-sw.js", "/static/precache-manifest.7a432dd021fffd9de9d07f
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
 const cacheName = 'BaseSite';
-const cacheVersion = '1582004205897ebf96edfe5cbd2c8aef8';
+const cacheVersion = '1582158711243ebf96edfe5cbd2c8aef8';
 const cacheKey = cacheName + cacheVersion;
 const urlsToCache = self.__precacheManifest.map(cacheItem => cacheItem.url);
 
